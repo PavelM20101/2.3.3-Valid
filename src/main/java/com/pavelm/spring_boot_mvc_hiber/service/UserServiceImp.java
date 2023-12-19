aa29558dfa5722c25f86bd1b1ecd6da693523709
Hozyain <qwpo01_0101@mail.ru>
2023-12-19 13:06:43 +0400

    Валидация

--- src/main/java/com/pavelm/spring_boot_mvc_hiber/service/UserServiceImp.java
+++ src/main/java/com/pavelm/spring_boot_mvc_hiber/service/UserServiceImp.java
@@ -25,7 +25,6 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
-    @Transactional
     public User getUser(int id) {
         return userDAO.getUser(id);
     }
@@ -51,7 +50,6 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
-    @Transactional
     public List<User> getAllUsers() {
         return userDAO.getAllUsers();
     }
